ff68f77b34d0c14ec3918f16b658fe1e3eb81d57
Relg99 <relg1999@gmail.com>
2020-08-31 22:46:28 -0500

    * Añadi todas la activities necesarias para el proyecto.

--- app/src/main/java/com/example/espacios/MainActivity.java
+++ app/src/main/java/com/example/espacios/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.espacios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button mybutton = (Button) findViewById(R.id.mybutton);
+        final TextView mytext = (TextView) findViewById(R.id.mytext);
+        final ConstraintLayout mylayout = (ConstraintLayout) findViewById(R.id.mylayout);
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/espacios/OptimizationActivity.java
@@ -0,0 +1,14 @@
+package com.example.espacios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptimizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optimization);
+    }
+}
